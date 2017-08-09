c2a823f0d7582b060bb27bdb6391561ca97a6d6d
siankatabg <siankata91@gmail.com>
2017-08-09 21:45:12 +0100

    Alert slider: Fix changing from "vibrate" to "all notifications" in some scenarios
    Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>

--- keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
+++ keyhandler/src/com/cyanogenmod/settings/device/KeyHandler.java
@@ -271,7 +271,14 @@ public class KeyHandler implements DeviceKeyHandler {
                     mAudioManager.setRingerModeInternal(sSupportedSliderModes.get(scanCode));
                 }
 
-                doHapticFeedback();
+		if (scanCode == MODE_NONE) {
+		    mAudioManager.setRingerModeInternal(sSupportedSliderModes.get(MODE_RING));
+		}
+		    
+
+		if (mVibrator != null) {
+		    mVibrator.vibrate(40);
+		}
             }
         } else if (!mEventHandler.hasMessages(GESTURE_REQUEST)) {
             Message msg = getMessageForKeyEvent(scanCode);
